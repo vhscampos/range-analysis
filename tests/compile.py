49521ebb3963de4dfef050a80b6dc3f4cd4c2568
igor.rafael <igor.rafael@fbcc87f1-ab0a-36ad-7d7d-49fb834a23a3>
2012-01-17 15:37:07 +0000

    Improved the compile script to run the CropDFS algorithm. It also have a better output to user.

--- tests/compile.py
+++ tests/compile.py
@@ -5,7 +5,7 @@ import os
 
 passPath = "/home/igor/workspace/llvm-3.0/Debug/lib/"
 
-if len(sys.argv) == 2:
+if len(sys.argv) >= 2:
     fileName, fileExtension = os.path.splitext(sys.argv[1])
     print("Compiling with Clang")
     print("clang -c -emit-llvm "+fileName+fileExtension+" -o "+fileName+".bc")
@@ -15,8 +15,12 @@ if len(sys.argv) == 2:
     os.system("opt -instnamer -mem2reg "+fileName+".bc -o "+fileName+".bc")
     print("opt -load "+passPath+"vSSA.so -vssa "+fileName+".bc -o "+fileName+".essa.bc")
     os.system("opt -load "+passPath+"vSSA.so -vssa "+fileName+".bc -o "+fileName+".essa.bc")
+    if len(sys.argv) == 3 and sys.argv[2] == "-crop":
+        raPass = sys.argv[2]
+    else:
+        raPass = ""
     print("Running the Range Analysis Pass")
-    print("opt -load "+passPath+"RangeAnalysis.so -range-analysis "+fileName+".essa.bc")
-    os.system("opt -load "+passPath+"RangeAnalysis.so -range-analysis "+fileName+".essa.bc")
+    print("opt -load "+passPath+"RangeAnalysis.so -range-analysis"+raPass+" "+fileName+".essa.bc")
+    os.system("opt -load "+passPath+"RangeAnalysis.so -range-analysis"+raPass+" "+fileName+".essa.bc")
 else:
-    print len(sys.argv)
+    print "./compile.py <FILE.c> [-crop]"
