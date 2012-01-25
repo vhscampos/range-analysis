2d246bf7c6a581c7283d010f75f5576fd1c867a6
igor.rafael <igor.rafael@fbcc87f1-ab0a-36ad-7d7d-49fb834a23a3>
2012-01-25 17:41:21 +0000

    Improved compile script.

--- tests/compile.py
+++ tests/compile.py
@@ -5,22 +5,36 @@ import os
 
 passPath = "/home/igor/workspace/llvm-3.0/Debug/lib/"
 
-if len(sys.argv) >= 2:
-    fileName, fileExtension = os.path.splitext(sys.argv[1])
+def checkArgs(args):
+    print ("Lenght=",len(args))
+    if len(args) != 3:
+        return False
+    if args[1] in ["-ra-inter-crop","-ra-inter-cousot","-ra-intra-crop","-ra-intra-cousot"]:
+        return True
+    else:
+        return False
+
+if checkArgs(sys.argv):
+    fileName, fileExtension = os.path.splitext(sys.argv[2])
     print("Compiling with Clang")
     print("clang -c -emit-llvm "+fileName+fileExtension+" -o "+fileName+".bc")
     os.system("clang -c -emit-llvm "+fileName+fileExtension+" -o "+fileName+".bc")
     print("Generating eSSA")
     print("opt -instnamer -mem2reg "+fileName+".bc -o "+fileName+".bc")
-    os.system("opt -instnamer -mem2reg "+fileName+".bc -o "+fileName+".bc")
+    os.system("opt -instnamer -mem2reg -inline -internalize "+fileName+".bc -o "+fileName+".bc")
     print("opt -load "+passPath+"vSSA.so -vssa "+fileName+".bc -o "+fileName+".essa.bc")
     os.system("opt -load "+passPath+"vSSA.so -vssa "+fileName+".bc -o "+fileName+".essa.bc")
-    if len(sys.argv) == 3 and sys.argv[2] == "-crop":
-        raPass = sys.argv[2]
-    else:
-        raPass = ""
     print("Running the Range Analysis Pass")
-    print("opt -load "+passPath+"RangeAnalysis.so -range-analysis"+raPass+" "+fileName+".essa.bc")
-    os.system("opt -load "+passPath+"RangeAnalysis.so -range-analysis"+raPass+" "+fileName+".essa.bc")
+    print("opt -load "+passPath+"RangeAnalysis.so "+sys.argv[1]+" "+fileName+".essa.bc")
+    os.system("opt -load "+passPath+"RangeAnalysis.so "+sys.argv[1]+" "+fileName+".essa.bc")
+
+#    os.system("opt -load "+passPath+"RangeAnalysis.so -load "+passPath+"Matching.so -matching "+fileName+".essa.bc")    
 else:
-    print "./compile.py <FILE.c> [-crop]"
+    print "./compile.py <OPTION> <FILE.c>"
+    print "\tOPTION:"
+    print "\t-ra-inter-crop\t\t-\tInter-procedural analysis with CropDFS meet operator"
+    print "\t-ra-inter-cousot\t-\tInter-procedural analysis with Cousot meet operator"
+    print "\t-ra-intra-crop\t\t-\tIntra-procedural analysis with CropDFS meet operator"
+    print "\t-ra-intra-cousot\t-\tIntra-procedural analysis with Cousot meet operator"
+
+
