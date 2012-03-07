7b9e171274319e24495335f38c185e60eda6d9c3
igor.rafael <igor.rafael@fbcc87f1-ab0a-36ad-7d7d-49fb834a23a3>
2012-03-07 13:02:49 +0000

    Improved the experiment.sh script to use SUITE and OUTPUT paths as arguments.

--- tests/experiment.sh
+++ tests/experiment.sh
@@ -12,90 +12,97 @@
 # 8: e-SSA, Cousot, Inter, inline
 #
 
-# Do not forget to put the ending / in every path below
-llvmpath=/work/victorsc/llvm-3.0/
-testpath=${llvmpath}projects/test-suite/External/SPEC/CINT2006/
-outputpath=/work/victorsc/output/main/
-build=Debug+Asserts/
+if [ $# -eq 2 ]; then
+	# Do not forget to put the ending / in every path below
+	llvmpath=/home/igor/workspace/ra/llvm-3.0/
+	testpath=${llvmpath}projects/test-suite/$1 #External/SPEC/CINT2006/
+	outputpath=$2#/home/igor/workspace/ra/experiments/
+	build=Debug/
 
-mkdir -p $outputpath
+	mkdir -p $outputpath
 
-# Experiment 1
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 1
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ANALYSIS=-ra-intra-cousot report report.csv
+	cd $testpath
+	make TEST=ra ANALYSIS=-ra-intra-cousot report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp1.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp1.csv
 
-# Experiment 2
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 2
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ESSA=1 ANALYSIS=-ra-intra-cousot report report.csv
+	cd $testpath
+	make TEST=ra ESSA=1 ANALYSIS=-ra-intra-cousot report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp2.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp2.csv
 
-# Experiment 3
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 3
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ANALYSIS=-ra-intra-cousot INLINE=1 report report.csv
+	cd $testpath
+	make TEST=ra ANALYSIS=-ra-intra-cousot INLINE=1 report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp3.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp3.csv
 
-# Experiment 4
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 4
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ESSA=1 ANALYSIS=-ra-intra-cousot INLINE=1 report report.csv
+	cd $testpath
+	make TEST=ra ESSA=1 ANALYSIS=-ra-intra-cousot INLINE=1 report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp4.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp4.csv
 
-# Experiment 5
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 5
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ANALYSIS=-ra-inter-cousot report report.csv
+	cd $testpath
+	make TEST=ra ANALYSIS=-ra-inter-cousot report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp5.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp5.csv
 
-# Experiment 6
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 6
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ESSA=1 ANALYSIS=-ra-inter-cousot report report.csv
+	cd $testpath
+	make TEST=ra ESSA=1 ANALYSIS=-ra-inter-cousot report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp6.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp6.csv
 
-# Experiment 7
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 7
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ANALYSIS=-ra-inter-cousot INLINE=1 report report.csv
+	cd $testpath
+	make TEST=ra ANALYSIS=-ra-inter-cousot INLINE=1 report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp7.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp7.csv
 
-# Experiment 8
-cd $llvmpath
-cd projects/test-suite
-touch TEST.ra.Makefile TEST.ra.report
+	# Experiment 8
+	cd $llvmpath
+	cd projects/test-suite
+	touch TEST.ra.Makefile TEST.ra.report
 
-cd $testpath
-make TEST=ra ESSA=1 ANALYSIS=-ra-inter-cousot INLINE=1 report report.csv
+	cd $testpath
+	make TEST=ra ESSA=1 ANALYSIS=-ra-inter-cousot INLINE=1 report report.csv
 
-mv ${testpath}report.ra.csv ${outputpath}exp8.csv
+	mv ${testpath}report.ra.csv ${outputpath}exp8.csv
+else
+	echo "How to use this script:"
+	echo "./experiment.sh <SUITE_PATH> <OUTPUT_PATH>"
+	echo "    <SUITE_PATH>: path where the test suite is located"
+	echo "    <OUTPUT_PATH>: path where the output results of the tests will be saved"
+fi
