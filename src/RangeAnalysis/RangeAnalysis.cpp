8d274a7027e918887c05d4234ca61b184a82ee46
vhscampos <vhscampos@fbcc87f1-ab0a-36ad-7d7d-49fb834a23a3>
2012-04-04 22:50:20 +0000

    Made some cleaning in code. Comments about and, or, xor updated.

--- src/RangeAnalysis/RangeAnalysis.cpp
+++ src/RangeAnalysis/RangeAnalysis.cpp
@@ -931,48 +931,19 @@ int64_t maxAND(int64_t a, int64_t b, int64_t c, int64_t d)
 	return b & d;
 }
 
+/*
+ *  This and operation was took from the original ConstantRange implementation.
+ *  It does not provide a tight result.
+ *  TODO: Implement Hacker's Delight algorithm
+ */
 Range Range::And(const Range& other) {
-//	const APInt &a = this->getLower();
-//	const APInt &b = this->getUpper();
-//	const APInt &c = other.getLower();
-//	const APInt &d = other.getUpper();
-//
-//	APInt candidates[4];
-//	candidates[0] = Min;
-//	candidates[1] = Min;
-//	candidates[2] = Max;
-//	candidates[3] = Max;
-//
-//	if (a.ne(Min) && c.ne(Min)) {
-//		candidates[0] = a.And(c); // lower lower
-//	}
-//
-//	if (a.ne(Min) && d.ne(Max)) {
-//		candidates[1] = a.And(d); // lower upper
-//	}
-//
-//	if (b.ne(Max) && c.ne(Min)) {
-//		candidates[2] = b.And(c); // upper lower
-//	}
-//
-//	if (b.ne(Max) && d.ne(Max)) {
-//		candidates[3] = b.And(d); // upper upper
-//	}
-//
-//	//Lower bound is the min value from the vector, while upper bound is the max value
-//	APInt *min = &candidates[0];
-//	APInt *max = &candidates[0];
-//
-//	for (unsigned i = 1; i < 4; ++i) {
-//		if (candidates[i].sgt(*max))
-//			max = &candidates[i];
-//		else if (candidates[i].slt(*min))
-//			min = &candidates[i];
-//	}
-//
-//	return Range(*min, *max);
+	if (isEmpty() || other.isEmpty())
+		return Range(Min, Max, Empty);
 
-	return Range(Min, Max);
+	APInt umin = APIntOps::umin(other.getUpper(), getUpper());
+	if (umin.isAllOnesValue())
+		return Range(Min, Max);
+	return Range(APInt::getNullValue(MAX_BIT_INT), umin);
 }
 
 int64_t minOR(int64_t a, int64_t b, int64_t c, int64_t d)
@@ -1024,6 +995,10 @@ int64_t maxOR(int64_t a, int64_t b, int64_t c, int64_t d)
 }
 
 
+/*
+ * 	This or operation is coded following Hacker's Delight algorithm.
+ * 	According to the author, it provides tight results.
+ */
 Range Range::Or(const Range& other) {
 	const APInt &a = this->getLower();
 	const APInt &b = this->getUpper();
@@ -1097,47 +1072,11 @@ int64_t maxXOR(int64_t a, int64_t b, int64_t c, int64_t d)
 	return maxOR(0, maxAND(a, b, ~d, ~c), 0, maxAND(~b, ~a, c, d));
 }
 
+/*
+ * 	We don't have a xor implementation yet.
+ * 	To be in safe side, we just give maxrange as result.
+ */
 Range Range::Xor(const Range& other) {
-//	const APInt &a = this->getLower();
-//	const APInt &b = this->getUpper();
-//	const APInt &c = other.getLower();
-//	const APInt &d = other.getUpper();
-//
-//	APInt candidates[4];
-//	candidates[0] = Min;
-//	candidates[1] = Min;
-//	candidates[2] = Max;
-//	candidates[3] = Max;
-//
-//	if (a.ne(Min) && c.ne(Min)) {
-//		candidates[0] = a.Xor(c); // lower lower
-//	}
-//
-//	if (a.ne(Min) && d.ne(Max)) {
-//		candidates[1] = a.Xor(d); // lower upper
-//	}
-//
-//	if (b.ne(Max) && c.ne(Min)) {
-//		candidates[2] = b.Xor(c); // upper lower
-//	}
-//
-//	if (b.ne(Max) && d.ne(Max)) {
-//		candidates[3] = b.Xor(d); // upper upper
-//	}
-//
-//	//Lower bound is the min value from the vector, while upper bound is the max value
-//	APInt *min = &candidates[0];
-//	APInt *max = &candidates[0];
-//
-//	for (unsigned i = 1; i < 4; ++i) {
-//		if (candidates[i].sgt(*max))
-//			max = &candidates[i];
-//		else if (candidates[i].slt(*min))
-//			min = &candidates[i];
-//	}
-//
-//	return Range(*min, *max);
-
 	return Range(Min, Max);
 }
 
